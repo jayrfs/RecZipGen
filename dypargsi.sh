324ef0055a535382d33bdcc735a61015c4a7380b
jayrfs <rfsjay@gmail.com>
2021-09-01 00:41:44 +0530

    🎨

--- dypargsi.sh
+++ dypargsi.sh
@@ -2,57 +2,59 @@
 
 # useless stuff
 
-echo "                                    "
-echo "                                    "
-echo "█████╗█████╗█████╗█████╗█████╗█████╗"
-echo "╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝"
-echo "                                    "
-sleep 0.1s
-echo "                                    "
-echo "                                    "
-echo "    ██████╗ ██╗   ██╗███╗   ██╗     "
-echo "    ██╔══██╗╚██╗ ██╔╝████╗  ██║     "
-echo "    ██║  ██║ ╚████╔╝ ██╔██╗ ██║     "
-echo "    ██║  ██║  ╚██╔╝  ██║╚██╗██║     "
-echo "    ██████╔╝   ██║   ██║ ╚████║     "
-echo "    ╚═════╝    ╚═╝   ╚═╝  ╚═══╝     "
-sleep 0.1s
-echo "                                    "
-echo "    ██████╗  █████╗ ██████╗         "
-echo "    ██╔══██╗██╔══██╗██╔══██╗        "
-echo "    ██████╔╝███████║██████╔╝        "
-echo "    ██╔═══╝ ██╔══██║██╔══██╗        "
-echo "    ██║     ██║  ██║██║  ██║        "
-echo "    ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝        "
-echo "                                    "
-sleep 0.1s
-echo "     ██████╗ ███████╗██╗            "
-echo "    ██╔════╝ ██╔════╝██║            "
-echo "    ██║  ███╗███████╗██║            "
-echo "    ██║   ██║╚════██║██║            "
-echo "    ╚██████╔╝███████║██║            "
-echo "     ╚═════╝ ╚══════╝╚═╝            "
-echo "                                    "
-echo "                                    "
-echo "                                    "
-echo "█████╗█████╗█████╗█████╗█████╗█████╗"
-echo "╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝"
-echo "                                    "
-echo "                                    "
+Banner=$(cat <<-END
+                                    
+█████╗█████╗█████╗█████╗█████╗█████╗
+╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝
+                                    
+    ██████╗ ██╗   ██╗███╗   ██╗     
+    ██╔══██╗╚██╗ ██╔╝████╗  ██║     
+    ██║  ██║ ╚████╔╝ ██╔██╗ ██║     
+    ██║  ██║  ╚██╔╝  ██║╚██╗██║     
+    ██████╔╝   ██║   ██║ ╚████║     
+    ╚═════╝    ╚═╝   ╚═╝  ╚═══╝     
+                                    
+    ██████╗  █████╗ ██████╗         
+    ██╔══██╗██╔══██╗██╔══██╗        
+    ██████╔╝███████║██████╔╝        
+    ██╔═══╝ ██╔══██║██╔══██╗        
+    ██║     ██║  ██║██║  ██║        
+    ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝        
+                                    
+     ██████╗ ███████╗██╗            
+    ██╔════╝ ██╔════╝██║            
+    ██║  ███╗███████╗██║            
+    ██║   ██║╚════██║██║            
+    ╚██████╔╝███████║██║            
+     ╚═════╝ ╚══════╝╚═╝            
+                                    
+█████╗█████╗█████╗█████╗█████╗█████╗
+╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝
+                                    
+END
+)
+
+while IFS= read -r line; 
+    do echo "$line"
+    sleep 0.05s
+done <<< "$Banner"
+
+
+
 sleep 0.1s
 #   make /storage/dynpargsi/images folder
 
 echo "Twrp flashable zip generator from gsi images for dynamic partition devices"
-echo ""
 sleep 0.1s
-echo "version 0.1"
-echo ""
+echo -e "\n\t\tversion 0.1\n"
+echo -e "\t  by @jayrfs"
+echo -e "  https://github.com/jayrfs/dynparGSI\n"
 sleep 1s
 echo "creating input directory.."
 echo ""
 #mkdir -p ~/storage/shared/dynpargsi/input
 sleep 0.1s
-echo "input directory created at /storage/dynpargsi/images"
+echo -e "input directory created at \n\t/storage/dynpargsi/images"
 echo "please place the images there"
 echo ""
 sleep 0.1s
