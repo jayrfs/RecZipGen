e3379f4ccdb2bc163f36c2a2e84304294e6ee408
jayrfs <rfsjay@gmail.com>
2021-09-01 01:05:31 +0530

    🎨

--- dypargsi.sh
+++ dypargsi.sh
@@ -44,18 +44,18 @@ done <<< "$Banner"
 sleep 0.1s
 #   make /storage/dynpargsi/images folder
 
-echo "Twrp flashable zip generator from gsi images for dynamic partition devices"
+echo -e "Twrp flashable zip generator from gsi images for dynamic partition devices"
 sleep 0.1s
 echo -e "\n\t\tversion 0.1\n"
-echo -e "\t  by @jayrfs"
-echo -e "  https://github.com/jayrfs/dynparGSI\n"
+echo -e "\t\tby @jayrfs\n"
+echo -e "\thttps://github.com/jayrfs/dynparGSI\n"
 sleep 1s
 echo "creating input directory.."
 echo ""
 #mkdir -p ~/storage/shared/dynpargsi/input
 sleep 0.1s
-echo -e "input directory created at \n\t/storage/dynpargsi/images"
-echo "please place the images there"
+echo -e "input directory created at \n\n\t/storage/dynpargsi/images"
+echo -e "\nplease place the images there"
 echo ""
 sleep 0.1s
 echo "done"
